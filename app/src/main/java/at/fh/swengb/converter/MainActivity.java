1bfdb1339e6bca68fe542f5aff679f54d99facc9
Florian Reinprecht <florian.reinprecht@edu.fh-joanneum.at>
2016-11-17 16:04:31 +0100

    About + Activity, DecimalFormat

--- app/src/main/java/at/fh/swengb/converter/MainActivity.java
+++ app/src/main/java/at/fh/swengb/converter/MainActivity.java
@@ -1,10 +1,12 @@
 package at.fh.swengb.converter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             gallon=Double.parseDouble(gallonString);
 
-            double result = gallon * 3.785411784;
+            String result = new DecimalFormat("##.##").format(gallon * 3.785411784);
             resultView.setText("Result: " + gallonString +" Galons = " + result + " Liters");
         } catch (NumberFormatException e) {
             resultView.setText("WRONG INPUT!!!");
@@ -45,13 +47,20 @@ public class MainActivity extends AppCompatActivity {
         try {
             liter=Double.parseDouble(literString);
 
-            double result = liter * 0.26417205235815;
+            String result = new DecimalFormat("##.##").format(liter * 0.26417205235815);
             resultView.setText("Result: " + literString +" Liters = " + result + " Galons");
         } catch (NumberFormatException e) {
             resultView.setText("WRONG INPUT!!!");
             e.printStackTrace();
         }
 
+    }
+
+    public void showAbout(View view) {
+        Intent intent = new Intent(this,DisplayAboutActivity.class);
+        String message = resultView.getText().toString();
+        intent.putExtra("extra",message);
+        startActivity(intent);
     }
 
 
